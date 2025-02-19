0a17195f7f5fd48edb485bd55ae4386b641788d5
Danilo Osorio <odanilo38@gmail.com>
2025-02-18 20:10:02 -0500

    Initial Commit

---
+++ EmpleadosMC/EmpleadosMCApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmpleadosMCApp.swift
+//  EmpleadosMC
+//
+//  Created by Danilo Osorio on 18/02/25.
+//
+
+import SwiftUI
+
+@main
+struct EmpleadosMCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
